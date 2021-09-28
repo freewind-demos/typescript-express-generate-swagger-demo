93786da3f1857bd4beed68a4bf033d93aa65af2e
Freewind <nowindlee@gmail.com>
2021-09-28 18:21:05 +0800

    typescript-express-generate-swagger-demo

--- README.md
+++ README.md
@@ -1,20 +1,13 @@
-TypeScript Express Demo
+TypeScript Express Generate Swagger Demo
 =======================
 
-Write express code in typescript.
+Use jsdoc with express to generate swagger api automatically.
+
+Question: How to find correct jsdoc param names and syntax? e.g. for body data
 
 ```
 npm install
 npm run demo
 ```
 
-Then visit <http://localhost:3000/hello1/typescript>, you will see `Hello1, typescript (GET)`.
-
-or
-
-```
-brew install httpie
-http POST http://localhost:3000/hello2 name=express
-```
-
-will see `Hello2, express (POST)`
+Then visit <http://localhost:3000/api-docs>
--- server.ts
+++ server.ts
@@ -4,12 +4,57 @@ import bodyParser from 'body-parser';
 const app = express();
 app.use(bodyParser.json());
 
+const expressSwagger = require('express-swagger-generator')(app);
+
+let options = {
+  swaggerDefinition: {
+    info: {
+      description: 'This is a sample server',
+      title: 'Swagger',
+      version: '1.0.0',
+    },
+    host: 'localhost:3000',
+    basePath: '/v1',
+    produces: [
+      "application/json",
+      "application/xml"
+    ],
+    schemes: ['http', 'https'],
+    // securityDefinitions: {
+    //   JWT: {
+    //     type: 'apiKey',
+    //     in: 'header',
+    //     name: 'Authorization',
+    //     description: "",
+    //   }
+    // }
+  },
+  basedir: __dirname, //app absolute path
+  files: ['server.ts'] //Path to the API handle folder
+};
+
+expressSwagger(options)
+
+/**
+ * @route GET /hello1/:name
+ * @group hello - hello to someone
+ * @param {string} name.query.required - Will say hello to this name
+ * @returns {object} 200 - Hello message to this name
+ * @returns {Error}  default - Unexpected error
+ */
 app.get('/hello1/:name', (req, res) => {
   const name = req.params.name;
   res.send(`Hello1, ${name} (GET)`);
 });
 
 // Notice: GET doesn't support request body, we should use POST
+/**
+ * @route POST /hello2
+ * @group hello - hello to someone
+ * @param {string} name.query.required - Will say hello to this name
+ * @returns {object} 200 - Hello message to this name
+ * @returns {Error}  default - Unexpected error
+ */
 app.post('/hello2', (req, res) => {
   const params = req.body as { name: string }
   res.send(`Hello2, ${params.name} (POST)`)
@@ -18,3 +63,5 @@ app.post('/hello2', (req, res) => {
 app.listen(3000, () => {
   console.log('listen on http://localhost:3000')
 });
+
+
